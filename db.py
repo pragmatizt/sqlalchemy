561b5b10a87c2378907e2b2d952456a611f50d86
Ira Evangelista <52584977+pragmatizt@users.noreply.github.com>
2020-03-18 19:57:29 -0400

    cleaned code

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, String, Integer, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -14,7 +14,7 @@ session = sessionmaker(bind=engine)()
 
 Base = declarative_base()
 
-# Define the user table in sqllite
+# Define the tower table in sqllite
 class Tower(Base):
     __tablename__ = "tower"  
 
@@ -26,29 +26,13 @@ class Tower(Base):
         self.password = password
 
 # =====================================
-# tower = Tower("att_juno", "phantom")
-# tower2 = Tower("sprint_wpb", "arnage")
+# tower = Tower('zoom_video_tower', 'password123')
 
 # session.add(tower)
-# session.add(tower2)
 
 # session.commit()
 # =====================================
+
 # Let's say we want to get usernames of all users in database.
 result = [t.towername for t in session.query(Tower).all()]
 from pdb import set_trace; set_trace()  # debugger
-
-# after, run python db.py 
-# type result.  Now you should see list of towernames
-
-# TERMINAL
-# =====================================
-# After adding a new tower, check on terminal
-# python db.py
-# sqlite3 celltower.db
-# select * from tower;
-# now see new results.
-
-# WARNING
-# ======================================
-# If new values are already in DB, will cause an error.
